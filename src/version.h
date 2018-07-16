d9ca1cd208c0eb3fec649762b31228cd4cb77090
Flapmin <Flapmin@Gmail.com>
2018-07-16 15:20:16 -0400

    Node ver

--- src/version.h
+++ src/version.h
@@ -30,7 +30,7 @@ static const int DATABASE_VERSION = 70509;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 1010000;
+static const int PROTOCOL_VERSION = 1010001;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
 static const int MIN_PROTO_VERSION = 209;
@@ -41,7 +41,7 @@ static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 1;
-static const int NOBLKS_VERSION_END = 999999;
+static const int NOBLKS_VERSION_END = 1009999;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
